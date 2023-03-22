62e7e1f66016236c49b53d23fe127c463c338dfb
Akintayo Durotoye <tayodurotoye@gmail.com>
2023-03-22 18:19:37 +0000

    geolacator sample

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const publicDirectoryPath = path.join(__dirname, "../pubic");
+
+app.use(express.static(publicDirectoryPath));
+
+app.get("/weather", (req, res) => {
+  res.send({
+    location: "Wolverhampton",
+    county: "west midlands, england, UK",
+    temperature: " 10 C",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on 3000");
+});
