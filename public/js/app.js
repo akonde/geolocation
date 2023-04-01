5b15c80f2a4729534a8e77de6d9c7d06a52b8f47
Akintayo Durotoye <tayodurotoye@gmail.com>
2023-04-01 17:50:02 +0100

    removed uncessary console log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Clients side javascript is load");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
